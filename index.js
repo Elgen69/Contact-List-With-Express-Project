379827230a8c367935203ac5e96cc08153f450e1
Elgen <23103623@usc.edu.ph>
2024-11-26 00:56:25 +0800

    Initial commit by Elgen ;)

---
+++ index.js
@@ -0,0 +1,33 @@
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+
+// Create express app
+const app = express();
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import the contact routes
+const contactRoutes = require("./src/routes/contact.route");
+
+// Setup server port
+const port = process.env.PORT || 6969;
+
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use the contact routes
+contactRoutes(app);
+
+// Define a root route to serve the index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html", "index.html"));
+});
+
+// Listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+});
